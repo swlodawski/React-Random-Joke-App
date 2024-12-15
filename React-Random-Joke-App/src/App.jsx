aeb9b3fab95d6daed2f917cb5e8c07a9e003e8b2
sammy wlodawski <samwlodawski@gmail.com>
2024-12-14 19:37:58 -0500

    Added components but need to figure out why joke componenet is undefined.

--- React-Random-Joke-App/src/App.jsx
+++ React-Random-Joke-App/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './components/Joke/Joke';
 function App() {
   
 
   return (
-    <>
-    </>
+    <div className='App'>
+      <h1>Random Joke Generator</h1>
+      <Joke />
+    </div>
   )
 }
 
---
+++ React-Random-Joke-App/src/components/Joke/Joke.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './Joke.css';
+import Button from '../Button/Button';
+
+
+const Joke = () => {
+
+    const [Joke, setJoke] = React.useState("");
+
+    const fetchApi = () => {
+        fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+        .then((res) => res.json()).then((data) => setJoke(data.joke));
+    };
+
+    return(
+        <div className="joke">
+            <Button callApi={fetchApi}></Button>
+        </div>
+    )
+}
+
+export default Joke;
